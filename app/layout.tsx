28cc8ff4875586bdad72282c46377e736e532dfc
manish <manishrajput6196@gmail.com>
2023-08-21 09:22:04 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from 'next'
+import { Nunito } from 'next/font/google'
+
+import './globals.css'
+import Navbar from './components/navbar/Navbar'
+import RegisterModal from './components/modals/RegisterModal'
+import LoginModal from './components/modals/LoginModal'
+import ToasterProvider from './providers/ToasterProvider'
+import getCurrentUser from './actions/getCurrentUser'
+import RentModal from './components/modals/RentModal'
+
+const nunito = Nunito({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Airbnb',
+  description: 'Airbnb clone',
+}
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const currentUser = await getCurrentUser()
+  return (
+    <html lang='en'>
+      <body className={nunito.className}>
+        <ToasterProvider />
+        <RegisterModal />
+        <LoginModal />
+        <RentModal />
+        <Navbar currentUser={currentUser} />
+        <div className='pb-20 pt-28'>{children}</div>
+      </body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,34 @@
+import getListings, { IListingsParams } from './actions/getListings'
+import Container from './components/Container'
+import EmptyState from './components/EmptyState'
+import ListingCard from './components/listings/ListingCard'
+import getCurrentUser from '@/app/actions/getCurrentUser'
+
+interface HomeProps {
+  searchParams: IListingsParams
+}
+
+const Home: React.FC<HomeProps> = async ({ searchParams }) => {
+  const listings = await getListings(searchParams)
+  const currentUser = await getCurrentUser()
+
+  if (listings?.length === 0) {
+    return <EmptyState />
+  }
+
+  return (
+    <Container>
+      <div className='pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
+        {listings.map(listing => (
+          <ListingCard
+            currentUser={currentUser}
+            key={listing.id}
+            data={listing}
+          />
+        ))}
+      </div>
+    </Container>
+  )
+}
+
+export default Home
